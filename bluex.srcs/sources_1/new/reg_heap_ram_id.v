6c9f6d95ef5fa8ed202da3625fa8c9125c061d5c
BirB.yeah <1635036180@qq.com>
2023-12-18 15:52:54 +0800

    delete comments

--- bluex.srcs/sources_1/new/reg_heap_ram_id.v
+++ bluex.srcs/sources_1/new/reg_heap_ram_id.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
 `include ".//global_macro.v"
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/12/18 14:26:39
-// Design Name: 
-// Module Name: reg_heap_ram_id
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module reg_heap_ram_id(
     input clk,

a9fedfb62a2247d54f791df22d25e6e69ae77d48
BirB.yeah <1635036180@qq.com>
2023-12-19 19:01:44 +0800

    first commit of compiler, can output machine code but with incomplete instruction collection

---
+++ bluex.srcs/sources_1/new/asm_c.py
@@ -0,0 +1,257 @@
+
+#TODO: complete the isc
+
+from argparse import ArgumentParser
+import os
+import sys
+
+R_set = set()
+I_set = set()
+J_set = set()
+
+GPR_ADR = 0
+ADR_BIT = 0
+IMM_BIT = 0
+OPC_BIT = 0
+
+version = "23.12.19.1.0"
+
+def show_version():
+    return version
+
+cli_parser = ArgumentParser(description='Calculate volume of a cylinder')
+cli_parser.add_argument("raw_file_name", help="the raw file name with its path")
+cli_parser.add_argument("-o", "--output", metavar="'name of output file'", help="output file position")
+cli_parser.add_argument('-v', '--version', action='version', version=show_version(),help='show version')
+cli_args = cli_parser.parse_args()
+
+macro_dict = dict()
+
+def info_print(messsage:str):
+	print(f"\033[0;93minfo:\033[0m {messsage}")
+
+def note_print(messsage:str):
+	print(f"\033[0;36mnote:\033[0m {messsage}")
+	
+def error_print(messsage:str):
+	print(f"\033[0;91merror:\033[0m {messsage}")
+
+def ce_print(mes:str, file=None, line_index:int=None, line_content:str=None):
+    if line_index is None:
+        print(f"\033[0;91mCompile error:\033[0m {mes}.")
+    else:
+        print(f"\033[0;91mCompile error in line {line_index + 1}:\033[0m {mes}.")
+    if file is not None:
+        file.seek(0)
+        print(f"error content:{file.readlines()[line_index]}")
+    sys.exit(1)
+
+def imm_proc(user_imm:str, f, line_index):
+	hex_flag = False
+	bin_flag = False
+	imm = None
+	imm_type = None
+	if user_imm[-1] == 'H':
+		user_imm = user_imm.removesuffix("H")
+		hex_flag = True
+	elif user_imm[-1] == 'B':
+		user_imm = user_imm.removesuffix("B")
+		bin_flag = True
+	try:
+		if not hex_flag and not bin_flag:
+			imm_type = ""
+			imm = int(user_imm)
+		elif hex_flag:
+			imm_type = "hex "
+			imm = int(user_imm, 16)
+		elif bin_flag:
+			imm_type = "bin "
+			imm = int(user_imm, 2)
+	except:
+		ce_print(f"unable to convert the given imm {user_imm} to {imm_type}number, please check it", f, line_index)
+	if imm >= (1 << IMM_BIT):
+		ce_print(f"converted imm is larger than {(1 << IMM_BIT) - 1}, consider reduce it", f, line_index)
+	return (imm & 0xffff)
+
+class isc_code:
+	OP_DISP = 26
+	RS_DISP = 21
+	RT_DISP = 16
+	RD_DISP = 11
+	SH_DISP = 6
+	FC_DISP = 0
+	IM_DISP = 0
+	AD_DISP = 0
+	def __init__(self) -> None:
+		self.isc = 0
+
+	def add_op(self, op_value:int, f, line_index):
+		if not isinstance(op_value, int):
+			ce_print(f"Compile error: op value must be a integer, but {type(op_value)} are given", f, line_index)
+		if op_value > ((1 << OPC_BIT) - 1) or op_value < 0:
+			ce_print(f"Compile error: op value must be less than {1 << OPC_BIT} and greater than 0, but {op_value} are given", f, line_index)
+		self.isc += op_value << isc_code.OP_DISP
+
+	def add_rs(self, rs_value:int, f, line_index):
+		if not isinstance(rs_value, int):
+			ce_print(f"Compile error: rs value must be a integer, but {type(rs_value)} are given", f, line_index)
+		if rs_value > ((1 << GPR_ADR) - 1) or rs_value < 0:
+			ce_print(f"Compile error: rs value must be less than {1 << GPR_ADR} and greater than 0, but {rs_value} are given", f, line_index)
+		self.isc += rs_value << isc_code.RS_DISP
+
+	def add_rt(self, rt_value:int, f, line_index):
+		if not isinstance(rt_value, int):
+			ce_print(f"Compile error: rt value must be a integer, but {type(rt_value)} are given", f, line_index)
+		if rt_value > ((1 << GPR_ADR) - 1) or rt_value < 0:
+			ce_print(f"Compile error: rt value must be less than {1 << GPR_ADR} and greater than 0, but {rt_value} are given", f, line_index)
+		self.isc += rt_value << isc_code.RT_DISP
+
+	def add_rd(self, rd_value:int, f, line_index):
+		if not isinstance(rd_value, int):
+			ce_print(f"Compile error: rd value must be a integer, but {type(rd_value)} are given", f, line_index)
+		if rd_value > ((1 << GPR_ADR) - 1) or rd_value < 0:
+			ce_print(f"Compile error: rd value must be less than {1 << GPR_ADR} and greater than 0, but {rd_value} are given", f, line_index)
+		self.isc += rd_value << isc_code.RD_DISP
+
+	def add_fc(self, fc_value:int, f, line_index):
+		if not isinstance(fc_value, int):
+			ce_print(f"Compile error: fc value must be a integer, but {type(fc_value)} are given", f, line_index)
+		if fc_value > ((1 << OPC_BIT) - 1) or fc_value < 0:
+			ce_print(f"Compile error: fc value must be less than {1 << OPC_BIT} and greater than 0, but {fc_value} are given", f, line_index)
+		self.isc += fc_value << isc_code.FC_DISP
+
+	def add_im(self, im_value:int, f, line_index):
+		if not isinstance(im_value, int):
+			ce_print(f"Compile error: im value must be a integer, but {type(im_value)} are given", f, line_index)
+		if im_value > ((1 << IMM_BIT) - 1) or im_value < 0:
+			ce_print(f"Compile error: im value must be less than {1 << IMM_BIT} and greater than 0, but {im_value} are given", f, line_index)
+		self.isc += im_value << isc_code.IM_DISP
+
+	def add_ad(self, ad_value:int, f, line_index):
+		if not isinstance(ad_value, int):
+			ce_print(f"Compile error: ad value must be a integer, but {type(ad_value)} are given", f, line_index)
+		if ad_value > ((1 << ADR_BIT) - 1) or ad_value < 0:
+			ce_print(f"Compile error: ad value must be less than {1 << ADR_BIT} and greater than 0, but {ad_value} are given", f, line_index)
+		self.isc += ad_value << isc_code.IM_DISP
+
+	def code(self, bin_str:bool=True):
+		if bin_str:
+			return bin(self.isc)[2:].zfill(32)
+		else:
+			return self.isc
+
+def load_macro():
+	global GPR_ADR, ADR_BIT, IMM_BIT, OPC_BIT
+	i = 0
+	if not os.path.exists("./global_macro.v"):
+		error_print("global_macro.v not found! Abort.")
+	with open("./global_macro.v") as f:
+		for line in f.readlines():
+			if line.startswith("`define ALO_"):
+				line = line.split()[1:4]
+				cmd = line[0].removeprefix("ALO_")
+				code = int(line[1].removeprefix("6'd"))
+				macro_dict[cmd] = code
+				cmd_type = line[2].removeprefix("//")
+				if cmd_type == "R":
+					R_set.add(cmd)
+				elif cmd_type == "I":
+					I_set.add(cmd)
+				else:
+					J_set.add(cmd)
+				# print(f"cmd: {cmd}\ttype:{cmd_type}\tcode:{code}")
+				i += 1
+			elif line.startswith("`define GPR_ADR"):
+				GPR_ADR = int(line.split()[2])
+				print(f"GPR ADR:{GPR_ADR}")
+			elif line.startswith("`define ADR_BIT"):
+				ADR_BIT = int(line.split()[2])
+				print(f"ADR BIT:{ADR_BIT}")
+			elif line.startswith("`define IMM_BIT"):
+				IMM_BIT = int(line.split()[2])
+				print(f"IMM BIT:{IMM_BIT}")
+			elif line.startswith("`define OPC_BIT"):
+				OPC_BIT = int(line.split()[2])
+				print(f"OPC BIT:{OPC_BIT}")
+	info_print(f"all isc found:{i} items") # should be 28 in this version
+	print(macro_dict)
+	
+
+def check_cli_arg_correctness():
+	if not os.path.exists(cli_args.raw_file_name):
+		print(f"raw file path provided({cli_args.raw_file_name}) is not a valid path, please check its completeness.")
+		sys.exit(1)
+	(main_path, extension) = os.path.splitext(cli_args.raw_file_name)
+	if not extension == ".txt":
+		print(f"raw file provided({cli_args.raw_file_name}) is not a file that supported by this complier, consider change its extension.")
+		sys.exit(1)
+	if cli_args.output == None:
+		cli_args.output = f"{main_path}_machine_code.txt"
+		with open(cli_args.output, "w") as file:
+			file.close() # this will clear the contents of the original file
+
+def compile():
+	with open(cli_args.raw_file_name) as f:
+		for (index, line) in enumerate(f.readlines()):
+			line = line.strip()
+			if line.startswith("#") or line.startswith("//"):
+				continue
+			else:
+				ic = isc_code()
+				line = line.upper().replace(",", "").split()
+				if line[0] in R_set:
+					if line[0] in ("ADD", "SUB", "ORL", "AND", "XOR", "SLL", "SRL", "SRA", "SLS"):
+						if len(line) != 4:
+							ce_print(f"cmd {line[0]} should be followed by 3 reg, but {len(line)} items are given", f, index)
+						for (reg_pos, reg) in enumerate(line[1:]):
+							if not reg.startswith("R"):
+								ce_print(f"expect reg, but \"{reg}\" found", f, index)
+							reg_num = reg[1:]
+							try:
+								reg_num = int(reg_num)
+							except:
+								ce_print(f"reg should have a form like \"R7\", but unrecognizable form {reg} are found", f, index)
+							if reg_pos == 0:
+								ic.add_rd(reg_num, f, index)
+							elif reg_pos == 1:
+								ic.add_rs(reg_num, f, index)
+							elif reg_pos == 2:
+								ic.add_rt(reg_num, f, index)
+						ic.add_fc(macro_dict[line[0]], f, index)
+				elif line[0] in I_set:
+					if line[0] in ("ADDI", "SUBI", "ORLI", "ANDI", "XORI", "BEQ", "BNE", "SLSI", "LDW", "SVW"):
+						if len(line) != 4:
+							ce_print(f"cmd {line[0]} should be followed by 2 reg and 1 imm, but {len(line)} items are given", f, index)
+					elif line[0] in ("NOTI", "MIRL", "MIRH"):
+						if len(line) != 3:
+							ce_print(f"cmd {line[0]} should be followed by 1 reg and 1 imm, but {len(line)} items are given", f, index)
+						for (rim_pos, rim) in enumerate(line[1:]):
+							if rim_pos == 0:
+								if not rim.startswith("R"):
+									ce_print(f"expect reg, but \"{rim}\" found", f, index)
+								rim_num = rim[1:]
+								try:
+									rim_num = int(rim_num)
+								except:
+									ce_print(f"rim should have a form like \"Rx\" where x is a number, but unrecognizable form {rim} are found", f, index)
+								ic.add_rt(rim_num, f, index)
+							elif rim_pos == 1:
+								ic.add_im(imm_proc(rim, f, index), f, index)
+					ic.add_op(macro_dict[line[0]], f, index)
+				elif line[0] in J_set:
+					if line[0] in ("JMP"):
+						if len(line) != 2:
+							ce_print(f"cmd {line[0]} should be followed by 1 imm, but {len(line)} items are given", f, index)
+						for (imm_pos, imm) in enumerate(line[1:]):
+							if imm_pos == 0:
+								ic.add_im(imm_proc(imm, f, index), f, index)
+					ic.add_op(macro_dict[line[0]], f, index)
+				with open(cli_args.output, "a") as output:
+					output.write(f"{ic.code()}\n")
+				print(ic.code())
+	
+if __name__ == "__main__":
+	load_macro()
+	check_cli_arg_correctness()
+	print(cli_args.raw_file_name, cli_args.output)
+	compile()
--- bluex.srcs/sources_1/new/global_macro.v
+++ bluex.srcs/sources_1/new/global_macro.v
@@ -7,7 +7,6 @@
 `define IMM_BIT			16				// immediate bit num
 `define OPC_BIT			6				// operation code bit num
 `define ERR_BIT			2
-`define SFT_BIT			5				// shift num bit
 
 `define RSF_RSF			2'b00			// rs_forward, select rs
 `define RSF_WBD			2'b01			// rs_forward, select write back data
@@ -25,51 +24,51 @@
 
 /** alu operation code **/
 // nothing
-`define ALO_NOP			6'd25			// do nothing, a bubble
+// `define ALO_NOP			6'd25			//R do nothing, a bubble
 /* arithmetic */
-`define ALO_ADD			6'd19			// R[rd]=R[rs]+R[rt]
-`define ALO_SUB			6'd18			// R[rd]=R[rs]-R[rt]
-`define ALO_ADDI		6'd26			// R[rt]=R[rs]+Imm
-`define ALO_SUBI		6'd27			// R[rt]=R[rs]-Imm
-`define ALO_MUL			6'd17			// don't implement
-`define ALO_DVM			6'd16			// don't implement
-`define ALO_MAC			6'd15			// don't implement
+`define ALO_ADD			6'd19			//R R[rd]=R[rs]+R[rt]
+`define ALO_SUB			6'd18			//R R[rd]=R[rs]-R[rt]
+`define ALO_ADDI		6'd26			//I R[rt]=R[rs]+Imm
+`define ALO_SUBI		6'd27			//I R[rt]=R[rs]-Imm
+// `define ALO_MUL			6'd17			// don't implement
+// `define ALO_DVM			6'd16			// don't implement
+// `define ALO_MAC			6'd15			// don't implement
 /* logic */
-`define ALO_NOT			6'd14			// R[rd]=~R[rs]
-`define ALO_ORL			6'd13			// R[rd]=R[rs]|R[rt]
-`define ALO_AND			6'd12			// R[rd]=R[rs]&R[rt]
-`define ALO_XOR			6'd11			// R[rd]=R[rs]^R[rt]
+`define ALO_NOT			6'd14			//R R[rd]=~R[rs]
+`define ALO_ORL			6'd13			//R R[rd]=R[rs]|R[rt]
+`define ALO_AND			6'd12			//R R[rd]=R[rs]&R[rt]
+`define ALO_XOR			6'd11			//R R[rd]=R[rs]^R[rt]
 //? must I implement these below? them can be replaced by two instruction
 // consider don't implement
-`define ALO_NOTI		6'd10			// R[rt]=~Imm
-`define ALO_ORLI		6'd9			// R[rt]=R[rs]|Imm
-`define ALO_ANDI		6'd8			// R[rt]=R[rs]&Imm
-`define ALO_XORI		6'd7			// R[rt]=R[rs]^Imm
+`define ALO_NOTI		6'd10			//I R[rt]=~Imm
+`define ALO_ORLI		6'd9			//I R[rt]=R[rs]|Imm
+`define ALO_ANDI		6'd8			//I R[rt]=R[rs]&Imm
+`define ALO_XORI		6'd7			//I R[rt]=R[rs]^Imm
 /* shift */
 // shift left logic
-`define ALO_SLL			6'd6			// R[rd]=R[rs]<<R[rt]
-`define ALO_SRL			6'd5			// R[rd]=R[rs]>>R[rt]
-`define ALO_SRA			6'd4			// R[rd]=R[rs]>>>R[rt]
+`define ALO_SLL			6'd6			//R R[rd]=R[rs]<<R[rt]
+`define ALO_SRL			6'd5			//R R[rd]=R[rs]>>R[rt]
+`define ALO_SRA			6'd4			//R R[rd]=R[rs]>>>R[rt]
 /* branch */
 // jump if equal
-`define ALO_BEQ			6'd3			// if(R[rs]==R[rt]) PC=PC+1+Imm
+`define ALO_BEQ			6'd3			//I if(R[rs]==R[rt]) PC=PC+1+Imm
 // jump if not equal
-`define ALO_BNE			6'd2			// if(R[rs]!=R[rt]) PC=PC+1+Imm
-`define ALO_JMP			6'd31			// PC=PC+1+Imm
+`define ALO_BNE			6'd2			//I if(R[rs]!=R[rt]) PC=PC+1+Imm
+`define ALO_JMP			6'd31			//J PC=PC+1+Imm
 /* compare */
 // set if little signed
-`define ALO_SLS			6'd1			// R[rd]=(R[rs]<R[rt]) ? 1 : 0
-`define ALO_SLSI		6'd0			// R[rt]=(R[rs]<Imm) ? 1 : 0
+`define ALO_SLS			6'd1			//R R[rd]=(R[rs]<R[rt]) ? 1 : 0
+`define ALO_SLSI		6'd0			//I R[rt]=(R[rs]<Imm) ? 1 : 0
 /* load/save */
-`define ALO_LDW			6'd20			// R[rt]=M[R[rs]+Imm]
-`define ALO_SVW			6'd21			// M[R[rs]+Imm]=R[rt]
+`define ALO_LDW			6'd20			//I R[rt]=M[R[rs]+Imm]
+`define ALO_SVW			6'd21			//I M[R[rs]+Imm]=R[rt]
 /* move */
 // move from reg to reg
-// `define ALO_MVRR		6'd22			// R[rt]=R[rs]
+// `define ALO_MVRR		6'd22			//R R[rt]=R[rs]
 // move imm to reg low 16 bits, without sign extension
-`define ALO_MIRL		6'd23			// R[rt]=Imm&0x0000ffff
+`define ALO_MIRL		6'd23			//I R[rt]=Imm&0x0000ffff
 // move imm to reg high 16 bits
-`define ALO_MIRH		6'd30			// R[rt]=Imm<<16
+`define ALO_MIRH		6'd30			//I R[rt]=Imm<<16
 
 /** error code **/
 `define ERR_NOE			`ERR_BIT'b00	// no error
